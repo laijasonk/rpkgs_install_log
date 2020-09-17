66a04764cca8cbfb9671bb953900a64e6ac8a69d
Jason K Lai <jklai@asu.edu>
2020-09-17 10:08:12 -0500

    Change paths to relative in clean_install.sh
    In order to safeguard against accidental deletion of files when a
    variable is misspelled, all paths have been changed to relative in the
    clean script.

--- bin/clean_install.sh
+++ bin/clean_install.sh
@@ -4,44 +4,39 @@
 #
 
 # Default values (TODO: add option flags?)
-src_dir="$(readlink -f ./src)"
-build_dir="$(readlink -f ./build)"
-lib_dir="$(readlink -f ./libs-r)"
-check_dir="$(readlink -f ./check)"
-log_dir="$(readlink -f ./log)"
+src_dir="./src"
+cran_dir="./src/cran"
+github_dir="./src/github"
+build_dir="./build"
+lib_dir="./libs-r"
+check_dir="./check"
+log_dir="./log"
+mkdir -p ./"${src_dir}" ./"${cran_dir}" ./"${github_dir}" ./"${build_dir}" ./"${lib_dir}" ./"${check_dir}" ./"${log_dir}"
 
 # Source files
 echo "Removing package source files: ${src_dir}"
-mkdir -p "${src_dir}" 
-cran_dir="$(readlink -f ./src/cran)"
-github_dir="$(readlink -f ./src/github)"
-mkdir -p "${cran_dir}" "${github_dir}"
-touch "${src_dir}/*.tar.gz" "${cran_dir}/tmp" "${github_dir}/tmp" && \
-    rm -R "${src_dir}"/*.tar.gz "${cran_dir}"/* "${github_dir}"/*
+touch ./"${src_dir}"/tmp.tar.gz ./"${cran_dir}"/tmp ./"${github_dir}"/tmp && \
+    rm -R -- ./"${src_dir}"/*.tar.gz ./"${cran_dir}"/* ./"${github_dir}"/*
 
 # Build files
 echo "Removing build files: ${build_dir}"
-mkdir -p "${build_dir}"
-touch "${build_dir}/tmp" && \
-    rm -R "${build_dir}"/*
+touch ./"${build_dir}"/tmp && \
+    rm -R -- ./"${build_dir}"/*
 
 # Library files
 echo "Removing local library files: ${lib_dir}"
-mkdir -p "${lib_dir}"
-touch "${lib_dir}/tmp" && \
-    rm -R "${lib_dir}"/*
+touch ./"${lib_dir}"/tmp && \
+    rm -R -- ./"${lib_dir}"/*
 
 # Check files
 echo "Removing checked packages: ${check_dir}"
-mkdir -p "${check_dir}"
-touch "${check_dir}/tmp" && \
-    rm -R "${check_dir}"/*
+touch ./"${check_dir}"/tmp && \
+    rm -R -- ./"${check_dir}"/*
 
 # Log files
 echo "Removing log files: ${log_dir}"
-mkdir -p "${log_dir}"
-touch "${log_dir}/tmp" && \
-    rm -R "${log_dir}"/*
+touch "${log_dir}"/tmp && \
+    rm -R -- "${log_dir}"/*
 
 # Nicer display
 echo
