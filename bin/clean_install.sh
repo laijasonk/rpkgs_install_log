e34a490d32e93b695ad7b0188853d96955c78448
Jason K Lai <jklai@asu.edu>
2020-09-17 09:55:53 -0500

    Corrected directory issue
    Directories are pointing in the wrong place in the clean_install.sh
    script. This is also a very dangerous script. It should be either made
    safer or removed entirely...

--- bin/clean_install.sh
+++ bin/clean_install.sh
@@ -5,8 +5,6 @@
 
 # Default values (TODO: add option flags?)
 src_dir="$(readlink -f ./src)"
-cran_dir="$(readlink -f ./src/cran)"
-github_dir="$(readlink -f ./src/github)"
 build_dir="$(readlink -f ./build)"
 lib_dir="$(readlink -f ./libs-r)"
 check_dir="$(readlink -f ./check)"
@@ -14,33 +12,36 @@ log_dir="$(readlink -f ./log)"
 
 # Source files
 echo "Removing package source files: ${src_dir}"
-mkdir -p "${src_dir}" "${cran_dir}" "${github_dir}"
-touch "${src_dir}/*.tar.gz" "${cran_dir}/tmp" "${github_dir}/tmp"
-rm -R "${src_dir}"/*.tar.gz "${cran_dir}"/* "${github_dir}"/*
+mkdir -p "${src_dir}" 
+cran_dir="$(readlink -f ./src/cran)"
+github_dir="$(readlink -f ./src/github)"
+mkdir -p "${cran_dir}" "${github_dir}"
+touch "${src_dir}/*.tar.gz" "${cran_dir}/tmp" "${github_dir}/tmp" && \
+    rm -R "${src_dir}"/*.tar.gz "${cran_dir}"/* "${github_dir}"/*
 
 # Build files
 echo "Removing build files: ${build_dir}"
 mkdir -p "${build_dir}"
-touch "${build_dir}/tmp"
-rm -R "${build_dir}"/*
+touch "${build_dir}/tmp" && \
+    rm -R "${build_dir}"/*
 
 # Library files
 echo "Removing local library files: ${lib_dir}"
 mkdir -p "${lib_dir}"
-touch "${lib_dir}/tmp"
-rm -R "${lib_dir}"/*
+touch "${lib_dir}/tmp" && \
+    rm -R "${lib_dir}"/*
 
 # Check files
 echo "Removing checked packages: ${check_dir}"
 mkdir -p "${check_dir}"
-touch "${check_dir}/tmp"
-rm -R "${check_dir}"/*
+touch "${check_dir}/tmp" && \
+    rm -R "${check_dir}"/*
 
 # Log files
 echo "Removing log files: ${log_dir}"
 mkdir -p "${log_dir}"
-touch "${log_dir}/tmp"
-rm -R "${log_dir}"/*
+touch "${log_dir}/tmp" && \
+    rm -R "${log_dir}"/*
 
 # Nicer display
 echo
