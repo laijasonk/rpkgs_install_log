1a0a590cbd204bfbac703aa227c21e845f04e8e7
Jason K Lai <laijasonk@gmail.com>
2020-10-06 21:15:09 -0500

    WIP: Added more potential download options
    Currently still a work-in-progress, but new options have been added as
    download options.

--- bin/download_and_build.sh
+++ bin/download_and_build.sh
@@ -8,14 +8,15 @@ curr_dir=$(pwd)
 # Help message
 function usage {
     echo "Usage: $0 -n name -v version -u http://buildurl -s 'buildfile'"
-    echo "       $0 -n name -v version -u http://giturl -s 'github' -c commit_hash"
+    echo "       $0 -n name -v version -u http://github/hash -s 'source'"
+    echo "       $0 -n name -v version -u http://cloneurl -s 'git' -c checkout_sha"
     echo "       $0 -n name -v version -u /path/to/url -s 'local'"
     echo "Flags:"
     echo "       -n package name"
     echo "       -v package version number"
     echo "       -u file url (if source=buildfile or github) or path to tarball (if source=local)"
-    echo "       -s source (currently supported: 'buildfile' or 'git' or 'local')"
-    echo "       -h github commit SHA hash"
+    echo "       -s source (currently supported: 'buildfile', 'source', 'git', or 'local')"
+    echo "       -h git commit SHA hash"
     echo "       -t OPTIONAL path to target directory"
     exit 1
 }
@@ -68,6 +69,8 @@ build_log="${log_dir}/build_${pkg_name}.txt"
 if [[ "${pkg_source}" == "buildfile" ]]
 then
     
+    # WARNING: CURRENTLY ASSUMES TAR.GZ
+
     # Define variables
     ext="tar.gz"
     pkg_archive="${build_dir}/${pkg_name}_${pkg_version}.${ext}" 
@@ -82,8 +85,10 @@ then
     echo "Extract step skipped because the build file was already provided for '${pkg_name}'" > "${extract_log}"
     echo "Build step skipped because the build file was already provided for '${pkg_name}'" > "${build_log}"
 
-elif [[ "${pkg_source}" == "github" ]]
+elif [[ "${pkg_source}" == "source" ]]
 then
+
+    # WARNING: CURRENTLY ASSUMES ZIP
 
     # Define variables
     ext="zip"
@@ -122,6 +127,53 @@ then
         echo "ERROR: Build tarball does not exist; build probably failed"
         exit 1
     fi
+
+elif [[ "${pkg_source}" == "git" ]]
+then
+
+    # ASSUMES pkg_url IS THE CLONE HTTP URL
+
+    # Define variables
+    pkg_archive="${src_dir}/${pkg_name}_${pkg_version}.${ext}" 
+    download_url="${pkg_url}/${git_commit}.zip"
+    pkg_build="${src_dir}/${pkg_name}-${git_commit}"
+
+    echo "Cloning '${pkg_name}' to '${src_dir}'"
+    cd "${src_dir}"
+    cmd="git clone \"${pkg_url}\" \"${pkg_build}/\""
+    echo -e "CMD: ${cmd}\n----------\n\n" > "${download_log}"
+    eval -- "${cmd}" >> "${download_log}" 2>&1
+    echo -e "\n\n----------\nExit status: $?">> "${download_log}"
+    cd "${curr_dir}"
+
+    echo "Checkout '${git_commit}' for '${pkg_name}'"
+    cd "${pkg_build}"
+    cmd="git checkout ${git_commit}"
+    echo -e "CMD: ${cmd}\n----------\n\n" > "${extract_log}"
+    eval -- "${cmd}" >> "${extract_log}" 2>&1
+    echo -e "\n\n----------\nExit status: $?">> "${extract_log}"
+    cd "${curr_dir}"
+ 
+    echo "Building '${pkg_name}' from '${src_dir}'"
+    cd "${src_dir}"
+    cmd="${rbinary} CMD build --no-build-vignettes \"${pkg_build}\""
+    echo -e "CMD: ${cmd}\n----------\n\n" > "${build_log}"
+    eval -- "${cmd}" >> "${build_log}" 2>&1
+    tarball="$(cat ${build_log} | sed '/^$/d' | tail -1 | sed -e 's/^.*‘//' -e 's/’.*$//')"
+    echo -e "\n\n----------\nExit status: $?">> "${build_log}"
+    cd "${curr_dir}"
+      
+    echo "Moving build tarball to '${build_dir}'"
+    src_tarball="${src_dir}/${tarball}"
+    build_tarball="${build_dir}/${tarball}"
+    if [[ -f "${src_tarball}" ]]
+    then
+        mv "${src_tarball}" "${build_tarball}"
+    else
+        echo "ERROR: Build tarball does not exist; build probably failed"
+        exit 1
+    fi
+
 
 elif [[ "${pkg_source}" == "local" ]]
 then
