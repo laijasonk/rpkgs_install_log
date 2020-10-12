00e55e85d5feb820f14cb608b2e47198afab937e
Jason K Lai <laijasonk@gmail.com>
2020-10-12 12:18:59 -0500

    Merge RDS summarization script into test script
    In order to keep the scripts more compact, the summarize_rds.sh was
    merged into the test_installed_package.sh script.

--- bin/test_installed_package.sh
+++ bin/test_installed_package.sh
@@ -36,10 +36,59 @@ fi
 # Test package
 echo "Testing '${pkg_name}' with testthat"
 test_log="${log_dir}/test_${pkg_name}.txt"
-test_dir="${lib_dir}/${pkg_name}/test"
+test_dir="${lib_dir}/${pkg_name}/tests"
 cmd="${rscript} -e \"testthat::test_dir('${test_dir}')\""
 echo -e "CMD: ${cmd}\n----------\n\n" > "${test_log}"
 eval -- "${cmd}" >> "${test_log}" 2>&1
 echo -e "\n\n----------\nExit status: $?">> "${test_log}"
 echo "Results saved to '${test_log}'"
+
+# Store RDS results to log
+rds_file="${lib_dir}/${pkg_name}/tests/unit_testing_results.rds"
+results_csv="${log_dir}/rds_${pkg_name}.txt"
+cat /dev/null > "${results_csv}"
+
+# Run R script to convert RDS results to CSV
+echo "Checking for rds file in '${rds_file}'"
+if [[ ! -f "$rds_file" ]]
+then
+    echo "No 'unit_testing_results.rds' file found in '${lib_dir}/${pkg_name}/tests'"
+    echo "WARNING: RDS file does not exist (${rds_file})" > ${results_csv}
+    exit 0
+fi
+
+# Run R code to convert rds results into CSV table
+echo "Converting rds file to '${results_csv}'"
+Rscript - <<EOF
+status_check_cross <- function(x) {
+    ifelse(is.null(x) | x == -1, "", ifelse((is.logical(x) & x == TRUE) | (is.numeric(x) & x == 0), "\\u2714", "\\u2718"))
+}
+
+library(testthat)
+rds_contents <- as.data.frame(readRDS(file.path('${rds_file}')))
+
+rds_contents\$file_context = paste0(rds_contents\$file, ": ", rds_contents\$context)
+
+output_df = data.frame()
+for (file_context in unique(rds_contents\$file_context)) {
+    context_data <- rds_contents[which(rds_contents\$file_context == file_context), ]
+    append_df <- data.frame(
+        file_context = file_context,
+        test = context_data\$test,
+        number_of_assertions = context_data\$nb,
+        failed = context_data\$failed,
+        skipped = context_data\$skipped,
+        error = context_data\$error,
+        warning = context_data\$warning,
+        pass = status_check_cross(context_data\$error == FALSE & context_data\$skipped == FALSE)
+    )
+    output_df <- rbind(output_df, append_df)
+}
+
+names(output_df) <- unique(rds_contents\$file_context)
+
+colnames(output_df) <- c("file_context", "test", "number_of_assertions", "failed", "skipped", "error", "warning", "pass")
+row.names(output_df) <- NULL
+write.csv(output_df, '${results_csv}')
+EOF
 
